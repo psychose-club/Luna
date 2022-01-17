a0620bd17c9c80e97b33e8ab474c9e6bf5c48311
CrashedLife <33805688+CrashedLife@users.noreply.github.com>
2022-01-17 17:35:17 +0100

    Added enums

---
+++ src/main/java/club/psychose/luna/enums/DiscordChannels.java
@@ -0,0 +1,5 @@
+package club.psychose.luna.enums;
+
+public enum DiscordChannels {
+    BOT_INFORMATION, LOGGING, VERIFICATION, ANY_CHANNEL
+}
---
+++ src/main/java/club/psychose/luna/enums/PermissionRoles.java
@@ -0,0 +1,5 @@
+package club.psychose.luna.enums;
+
+public enum PermissionRoles {
+    OWNER, ADMIN, MODERATOR, VERIFICATION, EVERYONE
+}
