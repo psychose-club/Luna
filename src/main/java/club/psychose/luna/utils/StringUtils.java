a7416dc1d018976e838c99c16b41c206ce466336
CrashedLife <33805688+CrashedLife@users.noreply.github.com>
2022-01-17 17:32:34 +0100

    Added StringUtils.java

---
+++ src/main/java/club/psychose/luna/utils/StringUtils.java
@@ -0,0 +1,32 @@
+package club.psychose.luna.utils;
+
+import java.io.File;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class StringUtils {
+    // Prints an empty line in the console.
+    public static void printEmptyLine () {
+        System.out.println(" ");
+    }
+
+    // These methods are for debugging.
+    public static void debug (String output) {
+        System.out.println(getDateAndTime("CONSOLE") + " | [Luna]: " + output);
+    }
+
+    // This method returns a date and time string.
+    public static String getDateAndTime (String formatMode) {
+        Date date = new Date();
+        DateFormat dateFormat = formatMode.equals("CONSOLE") ? new SimpleDateFormat("dd/MM/yyyy HH:mm:ss") : new SimpleDateFormat("dd-MM-yyyy HH-mm-ss");
+        return dateFormat.format(date);
+    }
+
+    // This returns a path with the valid file separators for the OS.
+    public static Path getOSPath (Path path) {
+        return Paths.get(path.toString().trim().replace("\\", File.separator));
+    }
+}
