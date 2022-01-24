145c2a8129c56acde1b43b6bcc42f98f0dc6ecb2
CrashedLife <33805688+CrashedLife@users.noreply.github.com>
2022-01-24 17:09:54 +0100

    Added new commands in CommandManager.java Added ClearTempFolderDiscordCommand.java Added ViewLogsDiscordCommand.java Added VerificationDiscordCommand.java

--- src/main/java/club/psychose/luna/core/system/managers/CommandManager.java
+++ src/main/java/club/psychose/luna/core/system/managers/CommandManager.java
@@ -1,19 +1,20 @@
 package club.psychose.luna.core.system.managers;
 
 import club.psychose.luna.core.bot.commands.DiscordCommand;
-import club.psychose.luna.core.bot.commands.executables.HelpDiscordCommand;
-import club.psychose.luna.core.bot.commands.executables.NukeDiscordCommand;
-import club.psychose.luna.core.bot.commands.executables.ReloadDiscordCommand;
+import club.psychose.luna.core.bot.commands.executables.*;
 
 import java.util.ArrayList;
 
-public class CommandManager {
+public final class CommandManager {
     private final ArrayList<DiscordCommand> discordCommandsArrayList = new ArrayList<>();
 
     public void initializeCommands () {
+        this.discordCommandsArrayList.add(new ClearTempFolderDiscordCommand());
         this.discordCommandsArrayList.add(new HelpDiscordCommand());
         this.discordCommandsArrayList.add(new NukeDiscordCommand());
         this.discordCommandsArrayList.add(new ReloadDiscordCommand());
+        this.discordCommandsArrayList.add(new ViewLogsDiscordCommand());
+        this.discordCommandsArrayList.add(new VerificationDiscordCommand());
     }
 
     public ArrayList<DiscordCommand> getDiscordCommandsArrayList () {
