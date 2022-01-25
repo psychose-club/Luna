22f9d6a7e730e6f98d48f2a787949e847d7e9cd6
CrashedLife <33805688+CrashedLife@users.noreply.github.com>
2022-01-25 16:03:50 +0100

    Added MusicPlayerTrackScheduler.java

---
+++ src/main/java/club/psychose/luna/core/bot/musicplayer/scheduler/MusicPlayerTrackScheduler.java
@@ -0,0 +1,144 @@
+package club.psychose.luna.core.bot.musicplayer.scheduler;
+
+import club.psychose.luna.utils.DiscordUtils;
+import com.sedmelluq.discord.lavaplayer.player.AudioPlayer;
+import com.sedmelluq.discord.lavaplayer.player.event.AudioEventAdapter;
+import com.sedmelluq.discord.lavaplayer.track.AudioTrack;
+import com.sedmelluq.discord.lavaplayer.track.AudioTrackEndReason;
+import net.dv8tion.jda.api.entities.Member;
+import net.dv8tion.jda.api.entities.TextChannel;
+import net.dv8tion.jda.api.entities.VoiceChannel;
+import net.dv8tion.jda.api.managers.AudioManager;
+
+import java.awt.*;
+import java.util.ArrayList;
+
+public final class MusicPlayerTrackScheduler extends AudioEventAdapter {
+    private final ArrayList<AudioTrack> musicPlayerQueueArrayList = new ArrayList<>();
+    private final AudioPlayer audioPlayer;
+    private final AudioManager audioManager;
+    private final VoiceChannel voiceChannel;
+    private TextChannel textChannel = null;
+
+    // Public constructor.
+    public MusicPlayerTrackScheduler (AudioPlayer audioPlayer, AudioManager audioManager, VoiceChannel voiceChannel)
+    {
+        // Initialize variables.
+        this.audioPlayer = audioPlayer;
+        this.audioManager = audioManager;
+        this.voiceChannel = voiceChannel;
+    }
+
+    // onTrackStart event.
+    @Override
+    public void onTrackStart (AudioPlayer audioPlayer, AudioTrack audioTrack) {
+        // Checks if the voice channel is not null.
+        if (this.voiceChannel != null) {
+            // Opens the audio connection.
+            this.voiceChannel.getGuild().getAudioManager().openAudioConnection(this.voiceChannel);
+
+            // Checks if the text channel is not null.
+            if (this.textChannel != null) {
+                // Sends an embedded message.
+                DiscordUtils.sendEmbedMessage(this.textChannel, "Music Bot", "I play now the following song:\n" + this.audioPlayer.getPlayingTrack().getInfo().title + "\n" + this.audioPlayer.getPlayingTrack().getInfo().author, null, "", Color.WHITE);
+            }
+        }
+    }
+
+    // onTrackEnd event.
+    @Override
+    public void onTrackEnd (AudioPlayer audioPlayer, AudioTrack audioTrack, AudioTrackEndReason audioTrackEndReason) {
+        // Checks if should mayStartNext.
+        if (audioTrackEndReason.mayStartNext) {
+            // Checks if the next track can be played.
+            if (this.checkIfNextTrackCanBePlayed()) {
+                // Plays the next track.
+                this.playNextTrack();
+            }
+        }
+    }
+
+    // Adds an AudioTrack to the queue.
+    public void addToQueue (AudioTrack audioTrack) {
+        // Checks if the audio track don't already exists in the queue (Only the variable, songs can be multiple added).
+        if (!(this.musicPlayerQueueArrayList.contains(audioTrack))) {
+            // Adds the audio track.
+            this.musicPlayerQueueArrayList.add(audioTrack);
+
+            // Checks if the next track can be played.
+            // If the next track can be played it'll be played.
+            if (this.checkIfNextTrackCanBePlayed())
+                this.playNextTrack();
+        }
+    }
+
+    // Plays the next track.
+    public boolean playNextTrack () {
+        // Checks if the queue size is bigger than 0.
+        if (this.musicPlayerQueueArrayList.size() > 0) {
+            // Gets the audio track.
+            AudioTrack nextAudioTrack = this.musicPlayerQueueArrayList.get(0);
+
+            // Removes the audio track from the queue.
+            this.musicPlayerQueueArrayList.remove(nextAudioTrack);
+
+            // Plays the audio track.
+            this.audioPlayer.playTrack(nextAudioTrack);
+
+            // Returns true.
+            return true;
+        }
+
+        // Returns false.
+        return false;
+    }
+
+    // Joins the voice channel.
+    public void joinVoiceChannel () {
+        // Checks if the voice channel is not null.
+        if (this.voiceChannel != null) {
+            // Checks if the bot is not already connected.
+            if (!(this.audioManager.isConnected())) {
+                // Opens the audio connection.
+                this.audioManager.openAudioConnection(this.voiceChannel);
+            }
+        }
+    }
+
+    // Stops the music bot.
+    public void stopMusicBot () {
+        // Clears the queue.
+        this.musicPlayerQueueArrayList.clear();
+
+        // Closes the audio connection.
+        this.audioManager.closeAudioConnection();
+    }
+
+    // Sets the new music channel variable.
+    public void resetMusicChannel (TextChannel value) {
+        this.textChannel = value;
+    }
+
+    // Checks if the next track can be played.
+    public boolean checkIfNextTrackCanBePlayed () {
+        // Returns if the track is null or not.
+        return this.audioPlayer.getPlayingTrack() == null;
+    }
+
+    // Checks if the member is in the same channel as the bot.
+    public boolean isInSameVoiceChannel (Member member) {
+        // Returns if the voice channel is not null and if the bot is connected to a voice channel and if the member is in the same voice channel.
+        return ((this.voiceChannel != null) && (this.isConnectedWithAnyVoiceChannel())) && (this.voiceChannel.getMembers().contains(member));
+    }
+
+    // Checks if the bot is connected to a voice channel.
+    public boolean isConnectedWithAnyVoiceChannel () {
+        // Returns if the bot is connected and if the connected channel is not null.
+        return ((this.audioManager.isConnected()) && (this.audioManager.getConnectedChannel() != null));
+    }
+
+    // Returns the queue.
+    public ArrayList<AudioTrack> getQueue () {
+        return this.musicPlayerQueueArrayList;
+    }
+}
