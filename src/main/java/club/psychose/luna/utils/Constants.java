250937c911194d6ef2c9b1ed7c2aecc0c9a56efe
CrashedLife <33805688+CrashedLife@users.noreply.github.com>
2022-02-04 22:09:38 +0100

    Upgrade Version 1.0.1 to Version 1.0.2

--- src/main/java/club/psychose/luna/utils/Constants.java
+++ src/main/java/club/psychose/luna/utils/Constants.java
@@ -21,8 +21,8 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 
 public final class Constants {
-    public static final String VERSION = "1.0.1";
-    public static final String BUILD = "2";
+    public static final String VERSION = "1.0.2";
+    public static final String BUILD = "3";
 
     public static Path getLunaFolderPath (String additionalPath) {
         return additionalPath != null ? StringUtils.getOSPath(Paths.get(System.getProperty("user.home") + "\\psychose.club\\Luna\\" + additionalPath)) : StringUtils.getOSPath(Paths.get(System.getProperty("user.home") + "\\psychose.club\\Luna\\"));
