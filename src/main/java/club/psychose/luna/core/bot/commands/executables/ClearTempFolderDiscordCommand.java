34cd42d653c8aaad2e4b72793037aca82ee6ea9a
CrashedLife <33805688+CrashedLife@users.noreply.github.com>
2022-03-03 17:01:00 +0100

    Implemented SlashCommand in ClearTempFolderDiscordCommand.java

--- src/main/java/club/psychose/luna/core/bot/commands/executables/ClearTempFolderDiscordCommand.java
+++ src/main/java/club/psychose/luna/core/bot/commands/executables/ClearTempFolderDiscordCommand.java
@@ -25,52 +25,76 @@ import club.psychose.luna.enums.DiscordChannels;
 import club.psychose.luna.enums.PermissionRoles;
 import club.psychose.luna.utils.Constants;
 import club.psychose.luna.utils.DiscordUtils;
+import net.dv8tion.jda.api.entities.Guild;
+import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent;
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
 
 import java.awt.*;
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
+import java.util.List;
 
 public final class ClearTempFolderDiscordCommand extends DiscordCommand {
     public ClearTempFolderDiscordCommand () {
-        super("cleartempfolder", "Clears the temporary folder on the server!", "!cleartempfolder", new String[] {"cltmp"}, new PermissionRoles[] {PermissionRoles.EVERYONE}, new DiscordChannels[] {DiscordChannels.ANY_CHANNEL});
+        super("cleartempfolder", "Clears the temporary folder on the bot server!", "!cleartempfolder", new String[] {"cltmp"}, new PermissionRoles[] {PermissionRoles.EVERYONE}, new DiscordChannels[] {DiscordChannels.ANY_CHANNEL}, "cleartempfolder", "Clears the temporary folder on the bot server!");
+    }
+
+    @Override
+    public void onSlashCommandExecution (SlashCommandInteractionEvent slashCommandInteractionEvent) {
+        if (slashCommandInteractionEvent.getMember() != null) {
+            if (slashCommandInteractionEvent.getMember().getId().equals("321249545394847747")) {
+                if (this.clearTempFolder(slashCommandInteractionEvent.getJDA().getGuilds())) {
+                    slashCommandInteractionEvent.getHook().editOriginalFormat("Temp folder cleared :)").queue();
+                } else {
+                    slashCommandInteractionEvent.getHook().editOriginalFormat("Temp folder already cleared :)").queue();
+                }
+            } else {
+                slashCommandInteractionEvent.getHook().editOriginalFormat("Invalid permissions!").queue();
+            }
+        }
     }
 
     @Override
     public void onCommandExecution (String[] arguments, MessageReceivedEvent messageReceivedEvent) {
         if (messageReceivedEvent.getAuthor().getId().equals("321249545394847747")) {
-            if (Files.exists(Constants.getLunaFolderPath("\\temp\\"))) {
-                File[] tempFiles = Constants.getLunaFolderPath("\\temp\\").toFile().listFiles();
-
-                if (tempFiles != null) {
-                    for (File file : tempFiles) {
-                        boolean isCaptchaFile = false;
-                        for (Captcha captcha : DiscordBot.CAPTCHA_MANAGER.getCaptchaArrayList()) {
-                            if (captcha.getImageFile().equals(file)) {
-                                isCaptchaFile = true;
-                                break;
-                            }
-                        }
-
-                        if (!(isCaptchaFile)) {
-                            try {
-                                Files.deleteIfExists(file.toPath());
-                            } catch (IOException ioException) {
-                                CrashLog.saveLogAsCrashLog(ioException, messageReceivedEvent.getJDA().getGuilds());
-                            }
-                        }
-                    }
-
-                    DiscordUtils.sendEmbedMessage(messageReceivedEvent.getTextChannel(), "Temp folder cleared :)", "Everything is clean now :o", null, "stay safe! <3", Color.GREEN);
-                } else {
-                    DiscordUtils.sendEmbedMessage(messageReceivedEvent.getTextChannel(), "Temp folder already cleared :)", "No need to let the bot do the dirty stuff.", null, "stay safe! <3", Color.GREEN);
-                }
+            if (this.clearTempFolder(messageReceivedEvent.getJDA().getGuilds())) {
+                DiscordUtils.sendEmbedMessage(messageReceivedEvent.getTextChannel(), "Temp folder cleared :)", "Everything is clean now :o", null, "stay safe! <3", Color.GREEN);
             } else {
                 DiscordUtils.sendEmbedMessage(messageReceivedEvent.getTextChannel(), "Temp folder already cleared :)", "No need to let the bot do the dirty stuff.", null, "stay safe! <3", Color.GREEN);
             }
         } else {
             DiscordUtils.sendEmbedMessage(messageReceivedEvent.getTextChannel(), "Invalid permissions!", "You didn't have permissions to execute this command!", null, "oh no qwq", Color.RED);
         }
+    }
+
+    private boolean clearTempFolder (List<Guild> guildList) {
+        if (Files.exists(Constants.getLunaFolderPath("\\temp\\"))) {
+            File[] tempFiles = Constants.getLunaFolderPath("\\temp\\").toFile().listFiles();
+
+            if (tempFiles != null) {
+                for (File file : tempFiles) {
+                    boolean isCaptchaFile = false;
+                    for (Captcha captcha : DiscordBot.CAPTCHA_MANAGER.getCaptchaArrayList()) {
+                        if (captcha.getImageFile().equals(file)) {
+                            isCaptchaFile = true;
+                            break;
+                        }
+                    }
+
+                    if (!(isCaptchaFile)) {
+                        try {
+                            Files.deleteIfExists(file.toPath());
+                        } catch (IOException ioException) {
+                            CrashLog.saveLogAsCrashLog(ioException, guildList);
+                        }
+                    }
+                }
+
+                return true;
+            }
+        }
+
+        return false;
     }
 }
