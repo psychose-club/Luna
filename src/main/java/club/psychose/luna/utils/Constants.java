936ee7b2b17e71be58f1a04427d18e24af5c860b
CrashedLife <33805688+CrashedLife@users.noreply.github.com>
2022-05-02 20:30:54 +0200

    Added GO_BACK_EMOJI in Constants.java

--- src/main/java/club/psychose/luna/utils/Constants.java
+++ src/main/java/club/psychose/luna/utils/Constants.java
@@ -17,12 +17,16 @@
 
 package club.psychose.luna.utils;
 
+import net.dv8tion.jda.api.entities.Emoji;
+
 import java.nio.file.Path;
 import java.nio.file.Paths;
 
 public final class Constants {
     public static final String VERSION = "1.0.3";
     public static final String BUILD = "4";
+
+    public static final Emoji GO_BACK_EMOJI = Emoji.fromUnicode("\uD83D\uDD19");
 
     public static Path getLunaFolderPath (String additionalPath) {
         return additionalPath != null ? StringUtils.getOSPath(Paths.get(System.getProperty("user.home") + "\\psychose.club\\Luna\\" + additionalPath)) : StringUtils.getOSPath(Paths.get(System.getProperty("user.home") + "\\psychose.club\\Luna\\"));
