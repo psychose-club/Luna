4f1f88444e07cb05583a84b4aca283131ac80eae
CrashedLife <33805688+CrashedLife@users.noreply.github.com>
2022-05-03 21:42:29 +0200

    Upgraded version from 1.0.3 to 1.0.4

--- src/main/java/club/psychose/luna/utils/Constants.java
+++ src/main/java/club/psychose/luna/utils/Constants.java
@@ -23,8 +23,8 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 
 public final class Constants {
-    public static final String VERSION = "1.0.3";
-    public static final String BUILD = "4";
+    public static final String VERSION = "1.0.4";
+    public static final String BUILD = "5";
 
     public static final Emoji GO_BACK_EMOJI = Emoji.fromUnicode("\uD83D\uDD19");
 
