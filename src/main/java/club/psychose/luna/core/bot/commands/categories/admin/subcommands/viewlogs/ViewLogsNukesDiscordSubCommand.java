a828dbc274b55863128f002307a5f790e9b00ce7
CrashedLife <33805688+CrashedLife@users.noreply.github.com>
2022-05-12 16:41:03 +0200

    Added ViewLogsNukesDiscordSubCommand.java

---
+++ src/main/java/club/psychose/luna/core/bot/commands/categories/admin/subcommands/viewlogs/ViewLogsNukesDiscordSubCommand.java
@@ -0,0 +1,50 @@
+/*
+ * Copyright © 2022 psychose.club
+ * Discord: https://www.psychose.club/discord
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package club.psychose.luna.core.bot.commands.categories.admin.subcommands.viewlogs;
+
+import club.psychose.luna.Luna;
+import club.psychose.luna.core.bot.commands.DiscordSubCommand;
+import club.psychose.luna.enums.FooterType;
+import club.psychose.luna.utils.Constants;
+import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
+
+import java.awt.*;
+import java.nio.file.Files;
+
+/*
+ * This is the subcommand class for the ViewLogs command.
+ */
+
+public final class ViewLogsNukesDiscordSubCommand extends DiscordSubCommand {
+    // Public constructor.
+    public ViewLogsNukesDiscordSubCommand () {
+        super("nuke", new String[] {"File Name"}, 1);
+    }
+
+    // Subcommand execution method.
+    @Override
+    public void onSubCommandExecution (String[] arguments, MessageReceivedEvent messageReceivedEvent) {
+        String fileName = arguments[0].trim();
+
+        if (Files.exists(Constants.getLunaFolderPath("\\logs\\nukes\\" + fileName))) {
+            messageReceivedEvent.getTextChannel().sendFile(Constants.getLunaFolderPath("\\logs\\nukes\\" + fileName).toFile()).queue();
+        } else {
+            Luna.DISCORD_MANAGER.getDiscordMessageBuilder().sendEmbedMessage(messageReceivedEvent.getTextChannel(), "File not found!", "We can't find the log file :(", FooterType.ERROR, Color.RED);
+        }
+    }
+}
